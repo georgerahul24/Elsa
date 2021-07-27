29d14cef43ecba035964b90701bbe3113478fc62
George Rahul <75750164+georgerahul24@users.noreply.github.com>
2021-07-28 01:55:55 +0530

    updated comments

--- GUI.py
+++ GUI.py
@@ -1,13 +1,9 @@
 '''Created by George Rahul'''
 
 from tkinter import *
-
-
-
 # Created by George Rahul
-
 def SecurityUI():
-
+    #.................initialising tkinter........................
     t = Tk()
     t.geometry("1x1")
     t.iconbitmap(r'icon.ico')
@@ -22,20 +18,21 @@ def SecurityUI():
     win.attributes("-alpha",0.8)
     win.iconbitmap(r'icon.ico')
     # win.overrideredirect(1)
-
+    #........entry fileds for username and password.............
     e = Entry(win, show="*", width=10)
     e.place(x=104, y=30)
-
     e1 = Entry(win, width=10)
     e1.place(x=104, y=10)
+
+    #..........Labels for username and password............................................
     t1 = Label(win, text="Username:", bg="light green", font="Nebula 10 bold").place(x=20, y=10)
     t2 = Label(win, text="Password:", bg="light green", font="Nebula 10 bold").place(x=20, y=30)
 
     win1 = Toplevel(t)
     win1.withdraw()
     win1.lift()
-    win1.attributes("-topmost", 1)
-    e3 = Entry(win1)
+    #win1.attributes("-topmost", 1)
+
 
 
     def password(event):
@@ -47,7 +44,8 @@ def SecurityUI():
 
 
     def button(a):
-        b = Button(win, text=a,bd=0, command=lambda:password("blah"))
+
+        b = Button(win, text=a,bd=0, command=lambda:password(""))
         b.place(x=90, y=60)
 
 
@@ -56,4 +54,5 @@ def SecurityUI():
     return password.usergui, password.passgui
 
 if __name__=="__main__":
-    SecurityUI()
+    a=SecurityUI()
+    print(a)
