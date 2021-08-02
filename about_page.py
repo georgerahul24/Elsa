d18b06417b9bb4966b1f0a80a4e1cd1b164a9021
George Rahul <75750164+georgerahul24@users.noreply.github.com>
2021-08-02 02:19:12 -0700

    Update about_page.py

--- about_page.py
+++ about_page.py
@@ -1,7 +1,7 @@
 from tkinter import *
 def about_page():
     ab=Tk()
-    h=Label(b,text="Created by:").pack()
+    h=Label(ab,text="Created by:").pack()
     g=Label(ab,text="Austin Bert").pack()
     a=Label(ab,text="George Rahul").pack()
     e=Label(ab,text="Elizabeth Jaison").pack()
