1b6a590c48f702eb2ab7631be930d21a63f30590
George Rahul <75750164+georgerahul24@users.noreply.github.com>
2021-08-11 18:47:59 +0530

    Update initial_setup.py

--- initial_setup.py
+++ initial_setup.py
@@ -1,9 +1,12 @@
 def install_files():
+    #Writing the default theme
     f = open('initial.vira', 'w')
     f.write(
-        "black;purple;light green \n #The order is bg,font color,button colour \n#Please remember to use ';' to separate colours :D"
+        "black;purple;light green\n#The order is bg,font color,button colour\n#Please remember to use ';' to separate colours :D"
     )
     f.close()
+    
+    #writing the users folder with default user admin and default password 1234
     f = open('users.vira', 'w')
     f.write("admin-1234")
     f.close()
