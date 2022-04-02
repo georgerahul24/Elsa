6950d69834cb633120674e1a8e35de6a2f8f3d0a
George Rahul <75750164+georgerahul24@users.noreply.github.com>
2022-04-02 18:02:33 +0530

    added auto reindexing of files if files are older than 3 days

--- elsabackend.py
+++ elsabackend.py
@@ -1,7 +1,10 @@
+import time
+
 from Magic import chat_client
 from Magic import history
 from talk1.talk1 import talk
 from task1 import task
+import os
 
 
 def quit(event = "") -> None:
@@ -29,3 +32,21 @@ def backend1_1(order: str) -> None:
     it to the history file"""
     backend1_1_dict[order][0]()
     history.user_file(order, backend1_1_dict[order][1])
+
+
+def maintenance_of_files():
+    def _compare_and_delete_files(filename):
+        file_createdtime = os.path.getmtime(filename)
+        currenttime = time.time()
+        time_lapse = (currenttime - file_createdtime) / (3600 * 24)  # Getting the number of days since file was modified
+        if time_lapse > 3:  # Delete the files if they are greater than 3 days old to re-index them again
+            os.remove(filename)
+
+    file_list = ['resources/ indexer.elsa', 'resources/ indexerfolder.elsa']
+    for file in file_list:
+        _compare_and_delete_files(file)
+
+
+try:
+    maintenance_of_files()
+except: pass
