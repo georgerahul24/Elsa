ca1f5920973fae9ae689b894b31e575e51e0f808
George Rahul <75750164+georgerahul24@users.noreply.github.com>
2022-07-14 23:31:18 +0530

    adding the indexer module
    - Have to implement multi threading

---
+++ Magic/Indexer.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Collections.Concurrent;
+using System.IO;
+namespace Magic
+{
+    public static class Indexer
+    {
+        public static ConcurrentDictionary<string,string> Index(string[] paths)
+        {
+            ConcurrentDictionary<string, string> PathDictionary = new();
+            foreach (string path in paths)
+            {
+                FindAllPaths(path, ref PathDictionary);
+            }
+
+            return PathDictionary;
+        }
+        private static void FindAllPaths(string path, ref ConcurrentDictionary<string, string> PathDictionary)
+        {
+            if (Directory.Exists(path))
+            {
+                
+                foreach (string childPath in Directory.GetDirectories(path))
+                {
+                    FindAllPaths(childPath, ref PathDictionary);
+                }
+                foreach (string childPath in Directory.GetFiles(path))
+                {
+                    PathDictionary[Path.GetFileName(childPath)] = childPath;
+                }
+            }
+            
+
+        }
+
+
+    }
+
+}
+
