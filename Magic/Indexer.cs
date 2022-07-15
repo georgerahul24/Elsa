980465f2eff65b885da0d15b588d99e2734a785a
George Rahul <75750164+georgerahul24@users.noreply.github.com>
2022-07-15 19:31:56 +0530

    Added multithreaded indexing

--- Magic/Indexer.cs
+++ Magic/Indexer.cs
@@ -5,29 +5,61 @@ namespace Magic
 {
     public static class Indexer
     {
-        public static ConcurrentDictionary<string,string> Index(string[] paths)
+       
+        private static readonly Json Json = new("Data.Json");
+        private static readonly ConcurrentDictionary<string, string> PathDictionary = new();
+        public static int  Index(string[] paths)
         {
-            ConcurrentDictionary<string, string> PathDictionary = new();
+            
+            List<Thread> mainProcesses = new();
             foreach (string path in paths)
             {
-                FindAllPaths(path, ref PathDictionary);
+                //Starting the threads
+                Thread thread = new Thread(()=>FindAllPaths(path));
+                mainProcesses.Add(thread);
+                Console.WriteLine($"{thread.ManagedThreadId} started");
+                thread.Start();
+                
             }
-
-            return PathDictionary;
+            //Joining the threads
+            foreach (Thread thread in mainProcesses)
+            {
+                thread.Join();
+                Console.WriteLine($"{thread.ManagedThreadId} stopped");
+            }
+            Json.Write(PathDictionary);
+            PathDictionary.Clear();//Resetting the dictionary
+            return 1;
         }
-        private static void FindAllPaths(string path, ref ConcurrentDictionary<string, string> PathDictionary)
+        private static void FindAllPaths(string path)
         {
             if (Directory.Exists(path))
             {
+                List<Thread> childProcesses = new();
+                try
+                {
+                    foreach (string childPath in Directory.GetDirectories(path))
+                    {
+                        Thread childThread = new Thread(()=>FindAllPaths(childPath));
+                        childProcesses.Add(childThread);
+                        childThread.Start();
+                    }
+                    foreach (string childPath in Directory.GetFiles(path))
+                    {
+                        PathDictionary[Path.GetFileName(childPath)] = childPath;
+                    }
+
+                    foreach (Thread childThread in childProcesses)
+                    {
+                        childThread.Join();
+                    }
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    Console.WriteLine($"Access denied to {path}");
+                    
+                }
                 
-                foreach (string childPath in Directory.GetDirectories(path))
-                {
-                    FindAllPaths(childPath, ref PathDictionary);
-                }
-                foreach (string childPath in Directory.GetFiles(path))
-                {
-                    PathDictionary[Path.GetFileName(childPath)] = childPath;
-                }
             }
             
 
